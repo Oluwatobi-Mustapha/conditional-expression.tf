598bf9553e08ab2cda578bcd1bd62343c8a045b2
Oluwatobi Mustapha <oluwatobimustapha539@gmail.com>
2025-10-20 22:17:43 +0100

    Change EC2 instance type based on conditions

--- ec2-conditional-expression.tf
+++ ec2-conditional-expression.tf
@@ -8,7 +8,7 @@ provider "aws" {
 
 resource "aws_instance" "myec2" {
   ami           = "ami-00c39f7145c08778" # Replace with your own AMI ID
-  instance_type = var.environment == "production" && var.region == "us-east-1" ? "t2.micro" : "m5.large"
+  instance_type = var.environment == "production" && var.region == "us-east-1" ? "m5.large" : "t2.micro"
 
   tags = {
     Name        = "Conditional-EC2"
